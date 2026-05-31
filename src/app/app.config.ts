c8de9f0f153230fcf6e5d59b8909fdaffc64fa4d
Obaid Taha <obaidramadan0@gmail.com>
2026-05-31 17:31:09 +0300

    update at thr interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MessageService } from 'primeng/api';
+// import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
+import { httpResponseInterceptor } from './core/interceptors/http-response/http-response-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,11 +21,12 @@ export const appConfig: ApplicationConfig = {
       },
     }),
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(),  withInterceptors([httpResponseInterceptor])),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     MessageService,
+    ConfirmationService,
     ToastModule,
   ],
 };
